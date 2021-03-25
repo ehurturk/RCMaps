85bd4b4990a8ed18757254b402761fa3066e5168
eZbal <79264523+eZbal@users.noreply.github.com>
2021-03-25 17:19:52 +0300

    Added app.js

--- App.js
+++ App.js
@@ -1,63 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import React, {useState} from 'react';
-import { StyleSheet, Text, View, Button, TextInput, CheckBox } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  Dimensions,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  CheckBox,
+  ImageBackground,
+} from "react-native";
 
 export default function App() {
-
   const [isSelected, setSelection] = useState(false);
   const [numTimes, setTimes] = useState(0);
-
   return (
-    <View style={styles.container}>
+    <ImageBackground
+      source={require("./RobertCollegeGouldHall1.jpg")}
+      style={styles.backgroundImage}
+    >
       <Text>RC Maps</Text>
       <Text>LOGIN PAGE</Text>
-      <br></br>
-      
       <View style={styles.inputArea}>
         <Text>Email: </Text>
         <TextInput placeholder="kardeş email nerde? "></TextInput>
 
         <Text>Password: </Text>
-        <TextInput  secureTextEntry={true}/>
+        <TextInput secureTextEntry={true} placeholder="parola nerde kardeşm?" />
       </View>
-
       <View style={styles.checkboxContainer}>
-        
-
         <CheckBox
           value={isSelected}
           onValueChange={setSelection}
           style={styles.checkbox}
-        
         />
         <Text style={styles.label}>AM I A PRO REACT DEV???</Text>
-
       </View>
-      
-      
       <Text>{isSelected ? "OW YE" : "O NO"}</Text>
-      <Button 
-      onPress={() => setTimes(numTimes+1)}
-      title = "LOG IN"
+      <Button
+        color="#7C0C1D"
+        onPress={() => setTimes(numTimes + 1)}
+        title="LOG IN"
       />
-      
-      <Text>You pressed the button {numTimes} times!</Text>
-
+      <Text style={styles.text}>You pressed the button {numTimes} times!</Text>
       <StatusBar style="auto" />
-    </View>
-
-   
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  // container: {
+  //   flex: 1,
+  //   backgroundColor: "#fff", // use camelCase
+  //   alignItems: "center",
+  //   justifyContent: "center",
+  // },
+
+  backgroundImage: {
     flex: 1,
-    backgroundColor: '#fff', // use camelCase
-    alignItems: 'center',
-    justifyContent: 'center',
-    
+    resizeMode: "cover",
+    alignItems: "center",
+    justifyContent: "center",
   },
 
   checkboxContainer: {
@@ -72,7 +75,5 @@ const styles = StyleSheet.create({
     margin: 8,
   },
 
-  inputArea: {
-
-  },
+  inputArea: {},
 });
