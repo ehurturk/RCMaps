44ecfc333cff87109177647e3703844ae1690510
eZbal <79264523+eZbal@users.noreply.github.com>
2021-04-08 17:18:50 +0300

    2. Sayfa
    2. sayfa zor iş bi el atın

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
 import {
@@ -11,41 +14,61 @@ import {
   ImageBackground,
 } from "react-native";
 
+const Stack = createStackNavigator();
+
 export default function App() {
   const [isSelected, setSelection] = useState(false);
   const [numTimes, setTimes] = useState(0);
-  return (
-    <ImageBackground
-      source={require("./RobertCollegeGouldHall1.jpg")}
-      style={styles.backgroundImage}
-    >
-      <Text>RC Maps</Text>
-      <Text>LOGIN PAGE</Text>
-      <View style={styles.inputArea}>
-        <Text>Email: </Text>
-        <TextInput placeholder="kardeş email nerde? "></TextInput>
+  const logInScreen = () => {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="LogIn" component={LogInScreen}>
+            {/* //Burayı çözemedim */}
+            <ImageBackground
+              source={require("./RobertCollegeGouldHall1.jpg")}
+              style={styles.backgroundImage}
+            >
+              <Text>RC Maps</Text>
+              <Text>LOGIN PAGE</Text>
+              <View style={styles.inputArea}>
+                <Text>Email: </Text>
+                <TextInput
+                  style={styles.textInput}
+                  placeholder="E-mail"
+                ></TextInput>
 
-        <Text>Password: </Text>
-        <TextInput secureTextEntry={true} placeholder="parola nerde kardeşm?" />
-      </View>
-      <View style={styles.checkboxContainer}>
-        <CheckBox
-          value={isSelected}
-          onValueChange={setSelection}
-          style={styles.checkbox}
-        />
-        <Text style={styles.label}>AM I A PRO REACT DEV???</Text>
-      </View>
-      <Text>{isSelected ? "OW YE" : "O NO"}</Text>
-      <Button
-        color="#7C0C1D"
-        onPress={() => setTimes(numTimes + 1)}
-        title="LOG IN"
-      />
-      <Text style={styles.text}>You pressed the button {numTimes} times!</Text>
-      <StatusBar style="auto" />
-    </ImageBackground>
-  );
+                <Text>Password: </Text>
+                <TextInput
+                  style={styles.textInput}
+                  secureTextEntry={true}
+                  placeholder="Password"
+                />
+              </View>
+              <View style={styles.checkboxContainer}>
+                <CheckBox
+                  value={isSelected}
+                  onValueChange={setSelection}
+                  style={styles.checkbox}
+                />
+                <Text style={styles.label}>Remember me</Text>
+              </View>
+              <Button
+                color="#7C0C1D"
+                onPress={() => setTimes(numTimes + 1)}
+                title="LOG IN"
+              />
+              <Text style={styles.text}>
+                You pressed the button {numTimes} times!
+              </Text>
+              <StatusBar style="auto" />
+            </ImageBackground>
+          </Stack.Screen>
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  };
+  logInScreen();
 }
 
 const styles = StyleSheet.create({
@@ -55,6 +78,11 @@ const styles = StyleSheet.create({
   //   alignItems: "center",
   //   justifyContent: "center",
   // },
+  textInput: {
+    borderColor: "black",
+    borderWidth: 1,
+    width: 250,
+  },
 
   backgroundImage: {
     flex: 1,
