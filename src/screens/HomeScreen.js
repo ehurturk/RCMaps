4c86abf860f4ce2203a10132e62cba6890d69b50
Emir Hurturk <emirhurturk444@gmail.com>
2021-11-27 21:02:02 +0300

    imgbackground added

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, ImageBackground, TouchableOpacity } from 'react-native';
-
 import { firebase } from "../firebase/config"
 
+const image = { uri: "https://reactjs.org/logo-og.png" };
 
 const HomeScreen = ({ navigation }) => {
 
@@ -28,33 +28,33 @@ const HomeScreen = ({ navigation }) => {
   return (
 
     <View style={styles.container}>
-      <Text style={styles.logo}>RCMaps</Text>
-      <View style={styles.inputView} >
-        <TextInput
-          style={styles.inputText}
-          placeholder="Email..."
-          placeholderTextColor="#003f5c"
-          onChangeText={text => setEmail(text)} />
-      </View>
-      <View style={styles.inputView} >
-        <TextInput
-          secureTextEntry
-          style={styles.inputText}
-          placeholder="Password..."
-          placeholderTextColor="#003f5c"
-          onChangeText={text => setpassword(text)} />
-      </View>
-      <TouchableOpacity>
-        <Text style={styles.forgot}>Forgot Password?</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.loginBtn} onPress={() => { login() }}>
-        <Text style={styles.loginText}>LOGIN</Text>
-      </TouchableOpacity>
-      <TouchableOpacity>
-        <Text style={styles.loginText} onPress={onFooterLinkPress}>Don't have an account? <Text onPress={onFooterLinkPress} style={styles.footerLink}>Sign up.</Text></Text>
-      </TouchableOpacity>
-
-
+        <ImageBackground source={require("../../assets/RobertCollegeGouldHall1.png")} resizeMode={"cover"} style={styles.image}>
+          <Text style={styles.logo}>RCMaps</Text>
+          <View style={styles.inputView} >
+            <TextInput
+              style={styles.inputText}
+              placeholder="Email..."
+              placeholderTextColor="#a1a1a1"
+              onChangeText={text => setEmail(text)} />
+          </View>
+          <View style={styles.inputView} >
+            <TextInput
+              secureTextEntry
+              style={styles.inputText}
+              placeholder="Password..."
+              placeholderTextColor="#a1a1a1"
+              onChangeText={text => setpassword(text)} />
+          </View>
+          <TouchableOpacity>
+            <Text style={styles.forgot}>Forgot Password?</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.loginBtn} onPress={() => { login() }}>
+            <Text style={styles.loginText}>LOGIN</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={styles.loginText} onPress={onFooterLinkPress}>Don't have an account? <Text onPress={onFooterLinkPress} style={styles.footerLink}>Sign up.</Text></Text>
+          </TouchableOpacity>
+      </ImageBackground>
     </View>
 
   );
@@ -65,19 +65,19 @@ export default HomeScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#003f5c',
-    alignItems: 'center',
+    // backgroundColor: '#003f5c',
+    // alignItems: 'center',
     justifyContent: 'center',
   },
   logo: {
     fontWeight: "bold",
     fontSize: 50,
-    color: "#fb5b5a",
-    marginBottom: 40
+    color: "#fff",
+    marginBottom: 40,
   },
   inputView: {
     width: "80%",
-    backgroundColor: "#465881",
+    backgroundColor: "#2a3357",
     borderRadius: 25,
     height: 50,
     marginBottom: 20,
@@ -108,5 +108,10 @@ const styles = StyleSheet.create({
   footerLink: {
     fontWeight: "bold",
     color: "#fb5b5a"
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: 'center'
   }
 });
